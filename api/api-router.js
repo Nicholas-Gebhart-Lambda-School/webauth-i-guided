da52ab9639fbb000ee592bf709feba3050ea1621
gebhartn <nicholas.gebhart@gmail.com>
2019-11-11 09:35:32 -0800

    feat: add hash

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post("/hash", (req, res) => {
+  // read password
+  // hash
+  // return to user in object that looks like:
+  // { password: 'original', hash: 'hashed'}
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.hash = hash;
+  res.status(200).json(credentials);
 });
 
 module.exports = router;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
 
   Users.add(user)
@@ -14,7 +14,7 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
@@ -23,7 +23,7 @@ router.post('/login', (req, res) => {
       if (user) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
